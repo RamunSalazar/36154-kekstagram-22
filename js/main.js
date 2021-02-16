fa9d0104f3d276006f554439201902ab2b3a772b
RamunSalazar <52279521+RamunSalazar@users.noreply.github.com>
2021-02-16 20:14:36 +0300

    Добавил модуль для отрисовки миниатюр

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import {isStringLength} from './util.js';
 import {generateArrayDescriptionPhotos} from './description-photo.js';
 import {generateCommentMessage} from './comment.js';
+import './thumbnail.js';
 
 isStringLength(generateCommentMessage().message);
 generateArrayDescriptionPhotos();
---
+++ js/thumbnail.js
@@ -0,0 +1,16 @@
+import {generateArrayDescriptionPhotos} from './description-photo.js';
+
+const pictures = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const fragment = document.createDocumentFragment();
+const thumbnailsPhoto = generateArrayDescriptionPhotos();
+
+thumbnailsPhoto.forEach((element) => {
+  const picture = pictureTemplate.cloneNode(true);
+  picture.querySelector('.picture__img').setAttribute('src', element.url);
+  picture.querySelector('.picture__likes').textContent = element.likes;
+  picture.querySelector('.picture__comments').textContent = element.comment.length;
+  fragment.appendChild(picture);
+});
+
+pictures.appendChild(fragment);
